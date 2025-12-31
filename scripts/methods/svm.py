Fixed svm metrics computation on val and test datasets

scripts/methods/svm.py
@@ -68,18 +68,15 @@ if __name__ == "__main__":
                     #
                     # Train metrics
                     acc_train = accuracy_score(Y_train, OUT_train)
-                    # f1_train = f1_score(Y_train, OUT_train, average='weighted')
                     precision_train, recall_train, f1_train, _ = precision_recall_fscore_support(y_true=Y_train, y_pred=OUT_train, average='weighted')
                     print(f'acc (train) = {acc_train}. f1 (train) = {f1_train}. precision (train) = {precision_train}. recall (train) = {recall_train}')
                     #
                     acc_val = accuracy_score(Y_val, OUT_val)
-                    # f1_val = f1_score(Y_val, OUT_val, average='weighted')
-                    precision_val, recall_val, f1_val, _ = precision_recall_fscore_support(y_true=Y_train, y_pred=OUT_train, average='weighted')
+                    precision_val, recall_val, f1_val, _ = precision_recall_fscore_support(y_true=Y_val, y_pred=OUT_val, average='weighted')
                     print(f'acc (val) = {acc_val}. f1 (val) = {f1_val}. precision (val) = {precision_val}. recall (val) = {recall_val}')
                     #
                     acc_test = accuracy_score(Y_test, OUT_test)
-                    # f1_test = f1_score(Y_test, OUT_test, average='weighted')
-                    precision_test, recall_test, f1_test, _ = precision_recall_fscore_support(y_true=Y_train, y_pred=OUT_train, average='weighted')
+                    precision_test, recall_test, f1_test, _ = precision_recall_fscore_support(y_true=Y_test, y_pred=OUT_test, average='weighted')
                     print(f'acc (test) = {acc_test}. f1 (test) = {f1_test}. precision (test) = {precision_test}. recall (test) = {recall_test}')
                     #
                     cm_train = confusion_matrix(Y_train, OUT_train)
@@ -104,15 +101,7 @@ if __name__ == "__main__":
     model_path = os.path.join(root_path, f"models/SVC/{exp_name}")
     os.makedirs(model_path, exist_ok=True)
     model_path = os.path.join(model_path, f"best_svc_configs.pkl")
-    #
-    # Read the data
-    X_train_split, Y_train, X_val_split, Y_val, X_test_split, Y_test = read_csv(file_path)
-    X_train_split = np.concatenate((X_train_split, X_val_split, X_test_split), axis=0)
-    Y_train = np.concatenate((Y_train, Y_val, Y_test), axis=0)
-    #
-    # Shuffle data
-    X_train_split, Y_train = shuffle(X_train_split, Y_train, random_state=42)
-    #
+    
     # Train the Model on the entire dataset and save
     best_model.fit(X_train_split, Y_train)
     with open(model_path, "wb") as f: